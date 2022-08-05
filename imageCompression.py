ecfac5ccbe51c36cb2f610a10d0f102752c79924
Linglai27 <30332630+Linglai27@users.noreply.github.com>
2022-08-05 15:29:37 -0400

    Update imageCompression.py

--- imageCompression.py
+++ imageCompression.py
@@ -4,19 +4,18 @@ from kmeansClustering import *
 
 img_location = "images/scene.png"
 original_img = plt.imread(img_location)
-print(original_img.shape)
 original_img = original_img / 255
 original_img_reshaped = np.reshape(original_img, (original_img.shape[0] * original_img.shape[1], original_img.shape[2]))
 
 # initialize parameters
-K = 8
-max_iteration = 8
+K = 2
+max_iteration = 2
 
 # choose initialization options
 initialization_methods = ["forgy", "random partition", "kmeans++"]
 
 # prepare for plots
-fig, ax = plt.subplots(1, len(initialization_methods) + 1, figsize=(8, 8))
+fig, ax = plt.subplots(1, len(initialization_methods) + 1, figsize=(16, 4))
 fig.suptitle("Comparison of K-means Image Compressions Using Different Initializations \n with {} clusters and {} "
              "iterations".format(K, max_iteration))
 plt.axis('off')
@@ -35,7 +34,7 @@ for i, initialization in enumerate(initialization_methods, 1):
     X_img = original_img_reshaped
     centroid, idx = kmeans_clustering(X_img, K, initialization, max_iteration)
     compressed_img = centroid[idx, :]
-    Error[i - 1] = np.amax(np.absolute(compressed_img - original_img_reshaped)) * 255
+    Error[i - 1] = np.amax(np.linalg.norm(compressed_img - original_img_reshaped, float('inf'))) * 255
     compressed_img = np.reshape(compressed_img, original_img.shape)
     ax[i].imshow(compressed_img * 255)
     ax[i].set_title('{}'.format(initialization))
